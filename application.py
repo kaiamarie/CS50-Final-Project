7e9b5b8927fc5657adc8e9e5701804dd3f0506f2
wagneran <lamm2101@msn.com>
2018-04-11 12:16:43 -0700

    added database creation to flask command line

--- application.py
+++ application.py
@@ -1,6 +1,29 @@
+import os
+from sqlite3 import dbapi2 as sqlite3
 from flask import Flask
+
 app = Flask(__name__)
 
+def get_db():
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        top.sqlite_db.row_factory = sqlite3.Row
+    return top.sqlite_db
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb'):
+def initdb_command():
+    if not os.path.exists('tmp/application.db'):
+        os.makedirs(os.path.dirname(app.config['DATABASE']), exist_ok = True)
+    init_db()
+    print('Initialized the database')
+
 @app.route('/')
-def hello_word():
+def index():
     return 'Hello, World!'
