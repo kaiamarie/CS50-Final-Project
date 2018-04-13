7557c858bd2e7180d849df85af1bffa736db97b8
kaiamarie <35552562+kaiamarie@users.noreply.github.com>
2018-04-13 15:11:59 -0700

    made a style sheet, basics of an html page

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_word():
-    return 'Hello, World!'
+def studentClass():
+    return render_template("studentClass.html")
