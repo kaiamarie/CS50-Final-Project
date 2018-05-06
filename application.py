9bb9216407448c85df2a4d1e4f83d9ba8759515c
kaiamarie <35552562+kaiamarie@users.noreply.github.com>
2018-05-06 10:32:38 -0700

    Registration, log in, and log out functional

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ import time
 
 app = Flask(__name__)
 app.config['DATABASE'] = 'tmp/application.db'
-
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 def get_db():
     top = _app_ctx_stack.top
@@ -31,8 +31,6 @@ def initdb_command():
     init_db()
     print('Initialized the database')
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-
 @app.route("/")
 @login_required
 def index():
@@ -49,32 +47,37 @@ def login():
 
         # Query database for username
         rows = get_db().execute("SELECT * FROM user WHERE username = ?",
-                          (request.form.get("username"))).fetchall()
+                          (request.form.get("username"),)).fetchone()
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["pw_hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        # Ensure username is registered
+        if len(rows) == 0:
+            return apology("Sorry, we couldn't find you. To access LessonThread, please register.")
+
+        # Ensure password is correct
+        if not check_password_hash(rows["pw_hash"], request.form.get("password")):
+            return apology("Sorry, wrong password!")
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows["id"]
+        print(session["user_id"])
 
         # run a check for adviser
         adviser_check = get_db().execute("SELECT * FROM adviser WHERE user_id = ?",
-                            (rows[0]["id"])).fetchall()
+                            (rows["id"],)).fetchone()
 
         # set session user type to adviser in order to show the adviser side of the site only
         if not adviser_check is None:
-            session["user_type"] = "adviser"
-            return redirect("adviser_home.html")
+            session["user_adviser"] = "true"
+            return render_template("adviser_home.html")
 
         # run a check for teacher
         teacher_check = get_db().execute("SELECT * FROM teacher WHERE user_id = ?",
-                            (rows[0]["id"])).fetchall()
+                            (rows["id"],)).fetchone()
 
         # session["user_type"] = "teacher"
         if not teacher_check is None:
-            session["user_type"] = "teacher"
-            return redirect("teacher_home.html")
+            session["user_teacher"] = "true"
+            return render_template("teacher_home.html")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -83,7 +86,7 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        print("tester")
+
         datetime = time.asctime(time.localtime(time.time()))
 
         # ensure username was unique
@@ -91,6 +94,7 @@ def register():
                         (request.form.get("email"),)).fetchall()
 
         if len(emailcheck) > 0:
+            print("its the emailcheck apology!")
             return apology("Email entered already registered")
 
         # ensure username was unique
@@ -100,11 +104,13 @@ def register():
         if len(rows) > 0:
             return apology("Username already registered")
 
-        
+        # save password
+        passwordhash = generate_password_hash(request.form.get("password"))
 
         # Insert username and hash of password into database (and auotgened ID)
         get_db().execute("INSERT INTO user (email, username, pw_hash, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
-                   (request.form.get("email"), request.form.get("username"), generate_password_hash(request.form.get("password")), datetime, datetime,)).fetchall()
+                   (request.form.get("email"), request.form.get("username"), passwordhash, datetime, datetime,))
+        get_db().commit()
 
         # add adviser/teacher information
         user_id = get_db().execute("SELECT id FROM user WHERE username = ?", (request.form.get("username"),)).fetchall()
@@ -114,23 +120,44 @@ def register():
         session["user_id"] = rows[0]["id"]
 
         newuser = get_db().execute("SELECT * FROM user WHERE username = ?", (request.form.get("username"),)).fetchall()
-        print(newuser)
 
-        # register advsers
+        # register advisers
         if request.form.get("user_type") == "adviser":
             get_db().execute("INSERT INTO adviser (user_id, created_at, updated_at) VALUES (?, ?, ?)",
-                    (user_id, datetime, datetime,)).fetchall()
+                    (user_id[0][0], datetime, datetime,))
+            get_db().commit()
             session["user_type"] = "adviser"
             print("adviser check")
-            return redirect("adviser_home.html")
+            return render_template("adviser_home.html")
 
 
         elif request.form.get("user_type") == "teacher":
             get_db().execute("INSERT INTO teacher (user_id, created_at, updated_at) VALUES (?, ?, ?)",
-                    (user_id, datetime, datetime,)).fetchall()
+                    (user_id[0][0], datetime, datetime,))
+            get_db().commit()
             session["user_type"] = "teacher"
             print("teacher check")
-            return redirect("teacher_home.html")
+            return render_template("teacher_home.html")
+        else:
+            return apology("Sorry, there was a problem registering you. Please try again.")
 
     else:
         return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/addclass", methods=["GET", "POST"])
+def addclass():
+    if request.method == "POST":
+
+
+    else:
+        return render_template("addclass.html")
--- helpers.py
+++ helpers.py
@@ -15,14 +15,5 @@ def login_required(f):
 
 def apology(message):
     """Renders message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", message=errormessage)
+    message = message
+    return render_template("errormessage.html", message=message)
